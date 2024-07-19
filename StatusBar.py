1467986e227553048f07cf6e050ee6e49726988e
Shine <jensen.yg@gmail.com>
2024-07-20 04:20:22 +0800

    StatusBar回到主线程执行

--- StatusBar.py
+++ StatusBar.py
@@ -1,4 +1,3 @@
-import threading
 
 
 class StatusBar:
@@ -18,12 +17,12 @@ class StatusBar:
         :param text: 文字
         :param duration: 持续时间，超过该时间后状态栏将清空。>0：时间，=0：无动作，保持原来的计时，-1：持续时间设定为永远。
         """
-        cls._runInDeamon(cls._set, text, duration)
-
-    @classmethod
-    def _set(cls, text: str = None, duration: int = -1):
         cls.labelWidget.config(text=text)
-        cls._setTimer(duration)
+        if duration != 0 and cls.timer:
+            cls.labelWidget.after_cancel(cls.timer)
+            cls.timer = None
+        if duration > 0:
+            cls.labelWidget.after(duration * 1000, cls.clear)
 
     @classmethod
     def append(cls, text: str, duration: int = -1):
@@ -32,33 +31,9 @@ class StatusBar:
         :param text: 文字
         :param duration: 持续时间，超过该时间后状态栏将清空。>0：时间，=0：无动作，保持原来的计时，-1：持续时间设定为永远。
         """
-        cls._runInDeamon(cls._append, text, duration)
+        cls.set(cls.labelWidget.cget('text') + text, duration)
 
     @classmethod
-    def _append(cls, text: str, duration: int = -1):
-        new_text = cls.labelWidget.cget('text') + text
-        cls.labelWidget.config(text=new_text)
-        cls._setTimer(duration)
-
-    @classmethod
-    def _runInDeamon(cls, func, *args):
-        """
-        使用守护线程来执行函数
-        由于状态栏显示涉及到Timer定时，有可能在程序结束时候阻止结束，因此使用守护线程执行，保证它会跟随主线程一同结束
-        """
-        t = threading.Thread(target=func, args=args)
-        t.daemon = True
-        t.start()
-
-    @classmethod
-    def _setTimer(cls, duration: int = None):
-        if duration != 0 and cls.timer:
-            cls.timer.cancel()
-        if duration > 0:
-            cls.timer = threading.Timer(duration, cls._onTimer)
-            cls.timer.start()
-
-    @classmethod
-    def _onTimer(cls):
+    def clear(cls):
+        """清空状态栏中的文字"""
         cls.labelWidget.config(text='')
-        cls.timer = None
